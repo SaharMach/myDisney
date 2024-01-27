a9b1adab1b473a32f373aadd06d1b81ef88466aa
SaharMach <saharmach28@gmail.com>
2024-01-27 22:44:25 +0200

    some deletes

--- disney-frontend/src/store/movies.action.js
+++ disney-frontend/src/store/movies.action.js
@@ -1,40 +1,11 @@
 import { store } from './store.js'
 import { SET_MOVIES,SET_MOVIE  } from "./movies.reducer.js";
 import { moviesService } from '../services/movies.serivce.js';
-// export function getActionRemoveBoard(boardId) {
-//     return {
-//         type: REMOVE_BOARD,
-//         boardId
-//     }
-// }
-// export function getActionAddBoard(board) {
-//     return {
-//         type: ADD_BOARD,
-//         board
-//     }
-// }
 
-// export function getActionUpdateBoard(board) {
-//     return {
-//         type: SET_BOARD,
-//         board
-//     }
-// }
 
 export async function loadMovies(studioId) {
     try {
         const movies = await moviesService.getById(studioId);
-
-        // let filteredBoards = boards
-        // if (user) {
-        //     if (user.username !== 'Guest') {
-        //         filteredBoards = boards.filter(board =>
-        //             board.members.some(boardMember => boardMember._id === user._id)
-        //         )
-        //     }
-        // } else {
-        //     console.log('No user provided, no boards will be filtered.');
-        // }
 
         store.dispatch({
             type: SET_MOVIES,
