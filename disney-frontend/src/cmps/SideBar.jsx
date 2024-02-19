4bcbca6fed2d7a9507e84940d0566b8144550a36
SaharMach <saharmach28@gmail.com>
2024-02-19 14:23:25 +0200

    fixing sidebar

--- disney-frontend/src/cmps/SideBar.jsx
+++ disney-frontend/src/cmps/SideBar.jsx
@@ -1,26 +1,36 @@
 import logo from '../assets/imgs/disneylogo.png'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 export function SideBar() {
     const user = useSelector(storeState => storeState.userModule.user)
     const [toggleMenu, setToggleMenu] = useState(false)
-    const activeClass = toggleMenu ? 'hide' : '';
+    const activeClass = toggleMenu ? '' : 'hide';
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 768) {
+                // This ensures the menu is closed on smaller screens
+                setToggleMenu(false);
+            }
+        }
+        window.addEventListener('resize', handleResize);
+        // Initial check to apply the logic when the component mounts
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <aside className="side-bar z-10 left-px justify-center z-50" >
             <Link to={'/'}>
                 <div className='logo'>
-
                     <img src="https://img.hotstar.com/image/upload/v1656431462/web-images/logo-d-plus-horizontal.svg" alt="" />
                 </div>
-
             </Link>
             <span onClick={() => setToggleMenu(!toggleMenu)} class="material-symbols-outlined menu">
                 menu
             </span>
             <div className={`navs-con ${activeClass}`}>
-
                 <section className="navs flex flex-col text-slate-300" >
                     <section className='w-full'>
                         <Link to={`${user ? '/watchlist' : '/login'}`}>
